c4187430e6893026578ef8b2707d24b1905da432
paper2parasol <paper2parasol@gmail.com>
2023-04-08 17:10:22 +0900

    サービスアカウントの認証を利用してアクセスできるようにする

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/paper2/proxy-cloud-run-go
 
 go 1.19
+
+require google.golang.org/api v0.116.0
+
+require (
+	cloud.google.com/go/compute v1.19.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230331144136-dcfb400f0633 // indirect
+	google.golang.org/grpc v1.54.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net/http"
-	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
 	"time"
+
+	"google.golang.org/api/idtoken"
 )
 
 type customRoundTripper struct {
@@ -16,40 +17,61 @@ type customRoundTripper struct {
 
 func (c customRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	// 認証処理
-	req.Header.Set("X-Forwarded-Proto", "test")
+	audience := "https://" + req.URL.Hostname()
+	if err := c.setAuthHeader(req, audience); err != nil {
+		log.Print(err)
+	}
 
 	start := time.Now()
 	resp, err := c.base.RoundTrip(req)
+
 	// log
 	log.Printf("%s %s %d %s, duration: %d", req.Method, req.URL.String(), resp.StatusCode, http.StatusText(resp.StatusCode), time.Since(start))
 	return resp, err
 }
 
+// OIDC TokenをAuth Headerに設定。
+func (c customRoundTripper) setAuthHeader(req *http.Request, audience string) error {
+	ctx := context.Background()
+	ts, err := idtoken.NewTokenSource(ctx, audience)
+	if err != nil {
+		log.Print("error: NewTokenSource is failed.")
+		return err
+	}
+	token, err := ts.Token()
+	if err != nil {
+		log.Print("error: failed to get token.")
+		return err
+	}
+	token.SetAuthHeader(req)
+	return nil
+}
+
 func main() {
 	// test 用のバックエンドサーバ
-	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		dump, err := httputil.DumpRequest(r, false)
-		if err != nil {
-			fmt.Fprintln(w, err)
-		}
-		fmt.Fprintln(w, string(dump))
-	}))
-	defer backendServer.Close()
+	// backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	dump, err := httputil.DumpRequest(r, false)
+	// 	if err != nil {
+	// 		fmt.Fprintln(w, err)
+	// 	}
+	// 	fmt.Fprintln(w, string(dump))
+	// }))
+	// defer backendServer.Close()
 
-	rpURL, err := url.Parse(backendServer.URL)
+	rpURL, err := url.Parse("https://test-service-sooyb7delq-an.a.run.app")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	director := func(req *http.Request) {
-		req.URL.Scheme = rpURL.Scheme
-		req.URL.Host = rpURL.Host
+	rewrite := func(r *httputil.ProxyRequest) {
+		r.SetXForwarded()
+		r.SetURL(rpURL)
 	}
 
 	// リクエストを受け取り、リバースプロキシを適用するハンドラーを定義する
 	proxy := &httputil.ReverseProxy{
 		Transport: customRoundTripper{base: http.DefaultTransport},
-		Director:  director,
+		Rewrite:   rewrite,
 	}
 	http.HandleFunc("/", proxy.ServeHTTP)
 	log.Fatal(http.ListenAndServe(":8080", nil))
